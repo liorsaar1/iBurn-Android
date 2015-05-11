1f77b0358ff70420db527129d119807a95b464f9
Lior Saar <liorsaar1@gmail.com>
2015-05-11 08:38:46 -0700

    Settings - ui

--- iBurn/src/main/java/com/gaiagps/iburn/fragment/SettingsFragment.java
+++ iBurn/src/main/java/com/gaiagps/iburn/fragment/SettingsFragment.java
@@ -1,25 +1,22 @@
 package com.gaiagps.iburn.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.text.method.ScrollingMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.gaiagps.iburn.R;
 import com.gaiagps.iburn.gj.message.GjMessage;
-import com.gaiagps.iburn.gj.message.GjMessageLighting;
 import com.gaiagps.iburn.gj.message.GjMessageMode;
 import com.gaiagps.iburn.gj.message.GjMessageReportGps;
 import com.gaiagps.iburn.gj.message.GjMessageRequestGps;
 import com.gaiagps.iburn.gj.message.GjMessageStatusRequest;
-import com.gaiagps.iburn.gj.message.GjMessageText;
 import com.google.android.gms.maps.model.LatLng;
 
 /**
@@ -28,13 +25,11 @@ import com.google.android.gms.maps.model.LatLng;
 public class SettingsFragment extends Fragment {
     private static final String TAG = "SettingsFragment";
 
-    private Spinner messageTypeSpinner;
-    private Spinner messageModeSpinner;
-    private View messageBufferModeContainer;
     private View messageEditTextContainer;
     private EditText messageEditText;
     private TextView messageConsole;
-    private Button messageSendButton;
+    private TextView messageIncoming;
+    private Button messageTextButton;
 
     public static SettingsFragment newInstance() {
         return new SettingsFragment();
@@ -54,93 +49,73 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-
-        messageBufferModeContainer = view.findViewById(R.id.GjBufferModeContainer);
-        messageEditTextContainer = view.findViewById(R.id.GjMessageEditText);
-        messageTypeSpinner = (Spinner) view.findViewById(R.id.GjMessageTypeSpinner);
-        messageTypeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                messageTypeOnItemSelected(position);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
-            }
-        });
-
-        messageModeSpinner = (Spinner) view.findViewById(R.id.GjBufferModeSpinner);
-
         messageEditText = (EditText) view.findViewById(R.id.GjMessageEditText);
-
-        messageSendButton = (Button) view.findViewById(R.id.GjMessageSendButton);
-        messageSendButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                messageSendOnClick();
-            }
-        });
-
         messageConsole = (TextView) view.findViewById(R.id.GjMessageConsole);
         messageConsole.setMovementMethod(new ScrollingMovementMethod());
+        messageIncoming = (TextView) view.findViewById(R.id.GjIncoming);
+        messageIncoming.setMovementMethod(new ScrollingMovementMethod());
+
+        view.findViewById(R.id.GjMessageStatusRequest).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onClickStatusRequest(v);
+            }
+        });
+        view.findViewById(R.id.GjMessageRequestGps).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { onClickRequestGps(v); }
+        });
+        view.findViewById(R.id.GjMessageReportGps).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { onClickReportGps(v); }
+        });
+        view.findViewById(R.id.GjMessageModeBuffered).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { onClickModeBuffered(v) ; }
+        });
+        view.findViewById(R.id.GjMessageModeNonBuffered).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) { onClickModeNonBuffered(v); }
+        });
+
         return view;
     }
 
     ///////////////////////////////////////////////
     // SEND
     ///////////////////////////////////////////////
+    Handler sendHandler = new Handler();
 
-    private void messageTypeOnItemSelected(int position) {
-        GjMessage.Type type = GjMessage.Type.values()[position];
-        messageEditTextContainer.setVisibility(View.INVISIBLE);
-        messageBufferModeContainer.setVisibility(View.INVISIBLE);
-        switch (type) {
-            case Text:
-                messageEditTextContainer.setVisibility(View.VISIBLE);
-                break;
-            case Mode:
-                messageBufferModeContainer.setVisibility(View.VISIBLE);
-                break;
-            case ReportGps:
-            case RequestGps:
-            case StatusRequest:
-            case Lighting:
-                break;
-        }
+    public void onClickStatusRequest(View v) {
+        send( new GjMessageStatusRequest() );
+    }
+    public void onClickRequestGps(View v) {
+        send( new GjMessageRequestGps() );
+    }
+    public void onClickReportGps(View v) {
+        int id = 5;
+        LatLng latLng = new LatLng(40.7888, -119.20315);
+        send(new GjMessageReportGps(id, latLng));
+    }
+    public void onClickModeBuffered(View v) {
+        send( new GjMessageMode(GjMessage.Mode.Buffered) );
+    }
+    public void onClickModeNonBuffered(View v) {
+        send( new GjMessageMode(GjMessage.Mode.NonBuffered) );
     }
 
-    private void messageSendOnClick() {
-        int typePosition = messageTypeSpinner.getSelectedItemPosition();
-        GjMessage.Type type = GjMessage.Type.values()[typePosition];
-        switch (type) {
-            case Mode:
-                int modePosition = messageModeSpinner.getSelectedItemPosition();
-                GjMessage.Mode mode = GjMessage.Mode.valueOf((byte)modePosition);
-                messageSend(new GjMessageMode(mode));
-                break;
-            case ReportGps:
-                LatLng latLng = new LatLng(40.7888, -119.20315);
-                int id = 5 ; // unit id
-                messageSend(new GjMessageReportGps(id, latLng));
-                break;
-            case RequestGps:
-                messageSend(new GjMessageRequestGps());
-                break;
-            case StatusRequest:
-                messageSend(new GjMessageStatusRequest());
-                break;
-            case Text:
-                String text = messageEditText.getText().toString().trim();
-                messageSend(new GjMessageText(text));
-                break;
-            case Lighting:
-                messageSend(new GjMessageLighting(""));
-                break;
-        }
+    private void send( final GjMessage message ) {
+        // queue requests to avoid ftdi jamming
+        sendHandler.post(new Runnable() {
+            @Override
+            public void run() {
+                console(message);
+            }
+        });
     }
 
-    private void messageSend(GjMessage message) {
+
+    private void console(GjMessage message) {
         messageConsole.append(">>> " + message.toString() + "\n");
         messageConsole.append(">>> " + message.toHexString() + "\n");
 
