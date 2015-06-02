e93ce9e7d0c221a2549ef6a913f89b194eaaac5e
Lior Saar <liorsaar1@gmail.com>
2015-06-01 22:18:34 -0700

    Light - seek bars reformat

--- iBurn/src/main/java/com/gaiagps/iburn/fragment/LightingFragment.java
+++ iBurn/src/main/java/com/gaiagps/iburn/fragment/LightingFragment.java
@@ -32,6 +32,30 @@ public class LightingFragment extends Fragment implements GjMessageListener {
     private static final String TAG = "LightingFragment";
     private static Button[] effectButton = new Button[8];
     private static SeekBar[] seekBar = new SeekBar[8];
+    private int effectSelected;
+
+    private View.OnClickListener effectOnClick = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            int index = Integer.parseInt((String) v.getTag());
+            effectButton[effectSelected].setSelected(false);
+            effectSelected = index;
+            v.setSelected(true);
+            onChange();
+        }
+    };
+    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            onChange();
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {}
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {}
+    };
 
     public static LightingFragment newInstance() {
         return new LightingFragment();
@@ -58,14 +82,14 @@ public class LightingFragment extends Fragment implements GjMessageListener {
         effectButton[5] = (Button) view.findViewById(R.id.lightEffect_2_2);
         effectButton[6] = (Button) view.findViewById(R.id.lightEffect_2_3);
         effectButton[7] = (Button) view.findViewById(R.id.lightEffect_2_4);
-        seekBar[0] = (SeekBar)view.findViewById(R.id.lightHue);
-        seekBar[1] = (SeekBar)view.findViewById(R.id.lightBrightness);
-        seekBar[2] = (SeekBar)view.findViewById(R.id.lightSaturation);
-        seekBar[3] = (SeekBar)view.findViewById(R.id.lightSpeed);
-        seekBar[4] = (SeekBar)view.findViewById(R.id.lightParameter1);
-        seekBar[5] = (SeekBar)view.findViewById(R.id.lightParameter2);
+        seekBar[0] = (SeekBar) view.findViewById(R.id.lightHue);
+        seekBar[1] = (SeekBar) view.findViewById(R.id.lightBrightness);
+        seekBar[2] = (SeekBar) view.findViewById(R.id.lightSaturation);
+        seekBar[3] = (SeekBar) view.findViewById(R.id.lightSpeed);
+        seekBar[4] = (SeekBar) view.findViewById(R.id.lightParameter1);
+        seekBar[5] = (SeekBar) view.findViewById(R.id.lightParameter2);
 
-        for (int i=0 ; i<8 ; i++) {
+        for (int i = 0; i < 8; i++) {
             effectButton[i].setOnClickListener(effectOnClick);
         }
 
@@ -75,35 +99,6 @@ public class LightingFragment extends Fragment implements GjMessageListener {
 
         return view;
     }
-
-    private int effectSelected;
-    private View.OnClickListener effectOnClick = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int index = Integer.parseInt((String) v.getTag());
-            effectButton[effectSelected].setSelected(false);
-            effectSelected = index;
-            v.setSelected(true);
-            onChange();
-        }
-    };
-
-    private SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
-        @Override
-        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-            onChange();
-        }
-
-        @Override
-        public void onStartTrackingTouch(SeekBar seekBar) {
-
-        }
-
-        @Override
-        public void onStopTrackingTouch(SeekBar seekBar) {
-
-        }
-    };
 
     private void onChange() {
         StringBuilder sb = new StringBuilder();
@@ -117,7 +112,7 @@ public class LightingFragment extends Fragment implements GjMessageListener {
     @Override
     public void onMessage(GjMessage message) {
         if (message instanceof GjMessageStatusResponse) {
-            GjMessageStatusResponse s = (GjMessageStatusResponse)message;
+            GjMessageStatusResponse s = (GjMessageStatusResponse) message;
             return;
         }
         if (message instanceof GjMessageFtdi) {
